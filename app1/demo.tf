5645f2209ae37d9883ac70172586cbd8f32e0140
vignesh ramkumar <vignesh.ramkumar@outlook.com>
2026-05-13 09:42:41 +0530

    this code is to create a server

---
+++ app1/demo.tf
@@ -0,0 +1,16 @@
+#create a server in aws 
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+    
